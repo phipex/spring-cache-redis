7eb0d862c6ea49f016ddd658a6651da75094038e
andres.montoya <andres.montoya@iesonline.com.co>
2023-10-11 16:30:29 -0500

    feat: nuevo proyecto

---
+++ src/main/java/co/com/ies/cachejhip/CachejhipApplication.java
@@ -0,0 +1,13 @@
+package co.com.ies.cachejhip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachejhipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachejhipApplication.class, args);
+	}
+
+}
